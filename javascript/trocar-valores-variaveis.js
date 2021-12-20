29fd8eda4d77952a34530f837579f13186c2f5da
Felipe Belisário <felipesantos.dsb@hotmail.com>
2021-12-19 22:44:40 -0300

    Add new exercise

---
+++ javascript/trocar-valores-variaveis.js
@@ -0,0 +1,13 @@
+// Mini Projeto 1 - Intensivo Dev Aprender
+// https://www.youtube.com/watch?v=i6Oi-YtXnAU&list=PL75H3gordSHxMtVaiwKTRT9QrxEuWyFIB&index=1&t=10s
+
+let a = 'vermelho';
+let b = 'azul';
+let c = '';
+
+c = a
+a = b
+b = c
+
+console.log('A = ' + a);
+console.log('B = ' + b);
