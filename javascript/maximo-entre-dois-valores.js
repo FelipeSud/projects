1785032d553d2c29e4486380690dd3b8fef6d159
Felipe Belisário <felipesantos.dsb@hotmail.com>
2021-12-21 17:21:17 -0300

    Add new project

---
+++ javascript/maximo-entre-dois-valores.js
@@ -0,0 +1,8 @@
+// Mini Projeto 2 - Intensivo Dev Aprender
+// https://www.youtube.com/watch?v=i6Oi-YtXnAU&list=PL75H3gordSHxMtVaiwKTRT9QrxEuWyFIB&index=1&t=10s
+let valorMaior = max(5, 9);
+console.log(valorMaior);
+
+function max(numero1, numero2) {
+    return numero1 > numero2 ? numero1: numero2;
+}
