cedba4e5ba1acf9275768ce325fc6d571538a86e
Felipe Belisário <felipesantos.dsb@hotmail.com>
2021-12-22 10:13:20 -0300

    Update radar project

--- javascript/medidor-de-velocidade.js
+++ javascript/medidor-de-velocidade.js
@@ -8,17 +8,17 @@ Caso os pontos sejam maior que 12 mostrar "Carteira Suspensa" */
 
 verificarVelocidade(200);
 
-function verificarVelocidade(velocidade){
+function verificarVelocidade(velocidade) {
     const velocidadeMaxima = 70;
     const kmPorPonto = 5;
     const limitePontos = 12
     if (velocidade <= velocidadeMaxima)
         console.log('Ok');
     else {
-     const pontos = Math.floor(((velocidade - velocidadeMaxima)/ kmPorPonto));
-     if (pontos >= limitePontos)
-     console.log('Carteira Suspensa');
-     else
-     console.log('Pontos', pontos);
+        const pontos = Math.floor(((velocidade - velocidadeMaxima) / kmPorPonto));
+        if (pontos >= limitePontos)
+            console.log('Carteira Suspensa');
+        else
+            console.log('Pontos', pontos);
     }
 }
---
+++ javascript/par-ou-impar.js
@@ -0,0 +1,5 @@
+// Mini Projeto 5 - Intensivo Dev Aprender
+// https://www.youtube.com/watch?v=i6Oi-YtXnAU&list=PL75H3gordSHxMtVaiwKTRT9QrxEuWyFIB&index=1&t=10s
+
+/* Receber uma quantidade de valores para avaliar
+Função exibe se cada valor é par ou ímpar */
