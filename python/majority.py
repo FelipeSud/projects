ca1a1ded3682f63a5bed73256a31cf64839edc6d
Felipe Belisário <felipesantos.dsb@hotmail.com>
2022-01-21 19:17:40 -0300

    Add majority.py

---
+++ python/majority.py
@@ -0,0 +1,8 @@
+# Faça um programa que receba a idade do usuário e diga se ele é maior ou menor de idade. 
+
+age = int(input('Olá, por favor insira sua idade: '))
+
+if age >= 18:
+    print('Você é maior de idade')
+else:
+    print('Você é menor de idade')
